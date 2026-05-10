5be676b9ced4999d533da698df9bcc0ce2ba457d
guiassys <guiassys@gmail.com>
2026-05-10 12:53:07 -0300

    fix: add cors config

--- src/main/java/com/apexlone/auraskill_api/security/SecurityConfig.java
+++ src/main/java/com/apexlone/auraskill_api/security/SecurityConfig.java
@@ -1,44 +1,30 @@
-package com.apexlone.auraskill_api.config;
+package com.apexlone.auraskill_api.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http)
+            throws Exception {
 
         http
-                // API stateless (JWT)
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-
-                // CSRF não necessário para API REST
+                .cors(cors -> {})
                 .csrf(csrf -> csrf.disable())
-
-                // Regras de acesso
                 .authorizeHttpRequests(auth -> auth
-
-                        // Swagger liberado (opcional)
-                        .requestMatchers(
-                                "/v3/api-docs/**",
-                                "/swagger-ui/**",
-                                "/swagger-ui.html"
-                        ).permitAll()
-
-                        // Exemplo: proteger tudo da API
-                        .requestMatchers("/api/**").authenticated()
-
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .anyRequest().authenticated()
                 )
-
-                // 🔥 ATIVA validação JWT via Keycloak
-                .oauth2ResourceServer(oauth2 -> oauth2
-                        .jwt(Customizer.withDefaults())
+                .oauth2ResourceServer(oauth2 ->
+                        oauth2.jwt(Customizer.withDefaults())
                 );
 
         return http.build();
