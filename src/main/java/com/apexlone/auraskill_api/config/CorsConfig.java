4a81b119e305fab83e9b4a48bcae20ac9c7740b8
guiassys <guiassys@gmail.com>
2026-05-16 12:44:23 -0300

    fix: remove hardcode parameter

--- src/main/java/com/apexlone/auraskill_api/config/CorsConfig.java
+++ src/main/java/com/apexlone/auraskill_api/config/CorsConfig.java
@@ -1,6 +1,7 @@
 package com.apexlone.auraskill_api.config;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -12,13 +13,15 @@ import java.util.List;
 @Configuration
 public class CorsConfig {
 
+    @Value("${app.cors.allowed-origins}")
+    private List<String> allowedOrigins;
+
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
 
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:3000",
-                "http://localhost:3001"));
+        configuration.setAllowedOrigins(allowedOrigins);
 
         configuration.setAllowedMethods(List.of(
                 "GET",
